31abdcad8d4a624f090fe5b49996d8a4805e4d8e
Heart_lin <lx617487285@163.com>
2019-08-31 20:37:29 +0800

    user

--- js/fina_fun.js
+++ js/fina_fun.js
@@ -7,3 +7,14 @@ function order_type(type){//1收入 2支出
 	}
 	return str;
 }
+function user_type(type){
+	var str="";
+	if(type==1){
+		str="管理员";
+	}else if(type==2){
+		str="出纳";
+	}else if(type==3){
+		str="报账人";
+	}
+	return str;
+}
